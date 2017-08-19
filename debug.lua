2fe008537fbbeed41537a4bd69273a65685321f2
Jaime Marques <mrthesoulz@gmail.com>
2017-08-19 16:05:34 +0100

    Total_Usage

--- debug.lua
+++ debug.lua
@@ -13,7 +13,7 @@ function NeP.Debug:Add(name, func, subroutines)
 	table.insert(self.Profiles, {
 		name = name,
 		func = func,
-		sr = subroutines,
+		subroutines = subroutines,
     max = 0,
     min = 0
 	})
@@ -21,12 +21,14 @@ end
 
 local tbl = NeP.Debug.Profiles
 C_Timer.NewTicker(1, function()
+  tbl.Total_Usage = 0
 	for i=1, #tbl do
-		local usage, calls = GetFunctionCPUUsage(tbl[i].func, tbl[i].sr)
+		local usage, calls = GetFunctionCPUUsage(tbl[i].func, tbl[i].subroutines)
 		tbl[i].cpu_time = usage
 		tbl[i].calls = calls
     tbl[i].max = tbl[i].max > usage and tbl[i].max or usage
     tbl[i].min = tbl[i].min < usage and tbl[i].min or usage
+    tbl.Total_Usage = tbl.Total_Usage + usage
 	end
 	ResetCPUUsage()
 end, nil)
