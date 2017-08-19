791969d3d1dff06da7fd104f1150621ea25d3357
Jaime Marques <mrthesoulz@gmail.com>
2017-08-19 16:48:46 +0100

    fix averages

--- debug.lua
+++ debug.lua
@@ -2,6 +2,8 @@ local _, NeP = ...
 
 NeP.Debug = {}
 NeP.Debug.Profiles = {}
+NeP.Debug.Profiles.total_usage = 0
+NeP.Debug.Profiles.total_average = 0
 
 SetCVar("scriptProfile", "1")
 
@@ -15,20 +17,27 @@ function NeP.Debug:Add(name, func, subroutines)
 		func = func,
 		subroutines = subroutines,
     max = 0,
-    min = 0
+    min = 0,
+    average = 0
 	})
+end
+
+local function GetAvg(a, b)
+  return a == 0 and b or (a+b)/2
 end
 
 local tbl = NeP.Debug.Profiles
 C_Timer.NewTicker(1, function()
-  tbl.Total_Usage = 0
+  tbl.total_usage = 0
 	for i=1, #tbl do
 		local usage, calls = GetFunctionCPUUsage(tbl[i].func, tbl[i].subroutines)
 		tbl[i].cpu_time = usage
 		tbl[i].calls = calls
+    tbl[i].average = GetAvg(tbl[i].average, usage)
     tbl[i].max = tbl[i].max > usage and tbl[i].max or usage
     tbl[i].min = tbl[i].min < usage and tbl[i].min or usage
-    tbl.Total_Usage = tbl.Total_Usage + usage
+    tbl.total_usage = tbl.total_usage + usage
+    tbl.total_average = GetAvg(tbl.total_average, tbl.total_usage)
 	end
 	ResetCPUUsage()
 end, nil)
