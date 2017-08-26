c62a4cc765e42ccf123721de6227d2be3b91c229
Andreas Nilsen <adde88@gmail.com>
2017-08-26 15:08:19 +0200

    Added two new item-conditons: usable + cooldown (#264)
    I added these two conditions, to have a way of checking if a specific item is on cooldown, how long the cooldown is, and to check if an item is usable.
    
    Example usage:
    Some routines keeps trying to use Healthstones without checking if it's actually usable.

--- conditions/items.lua
+++ conditions/items.lua
@@ -8,6 +8,18 @@ NeP.DSL:Register({'equipped', 'item'}, function(_, item)
   return IsEquippedItem(item)
 end)
 
+NeP.DSL:Register('item.cooldown', function(_, item)
+	local start, duration = GetItemCooldown(item)
+	if not start then return 0 end
+  return start ~= 0 and (start + duration - GetTime()) or 0
+end)
+
+NeP.DSL:Register('item.usable', function(_, item)
+	local isUsable, notEnoughMana = IsUsableItem(item)
+  if not isUsable or notEnoughMana or NeP.DSL:Get('item.cooldown')(nil, item) ~= 0 then return false end
+	return true
+end)
+
 NeP.DSL:Register('item.count', function(_, item)
   return GetItemCount(item, false, true)
 end)
