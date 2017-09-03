30802a8273a4d272823337d690ee437ff54dfee9
Jaime Marques <mrthesoulz@gmail.com>
2017-09-03 20:49:18 +0100

    move heal conditions

---
+++ conditions/heal.lua
@@ -0,0 +1,52 @@
+local _, NeP = ...
+
+NeP.DSL:Register("health", function(target)
+	return NeP.Healing.healthPercent(target)
+end)
+
+NeP.DSL:Register("health.actual", function(target)
+	return _G.UnitHealth(target)
+end)
+
+NeP.DSL:Register("health.max", function(target)
+	return _G.UnitHealthMax(target)
+end)
+
+NeP.DSL:Register("health.predicted", function(target)
+	return NeP.Healing.GetPredictedHealth_Percent(target)
+end)
+
+NeP.DSL:Register("health.predicted.actual", function(target)
+	return NeP.Healing.GetPredictedHealth(target)
+end)
+
+-- USAGE: UNIT.area(DISTANCE, HEALTH).heal >= #
+NeP.DSL:Register("area.heal", function(unit, args)
+	local total = 0
+	if not _G.UnitExists(unit) then return total end
+	local distance, health = _G.strsplit(",", args, 2)
+	for _,Obj in pairs(NeP.Healing:GetRoster()) do
+		local unit_dist = NeP.Protected.Distance(unit, Obj.key)
+		if unit_dist < (tonumber(distance) or 20)
+		and Obj.health < (tonumber(health) or 100) then
+			total = total + 1
+		end
+	end
+	return total
+end)
+
+-- USAGE: UNIT.area(DISTANCE, HEALTH).heal.infront >= #
+NeP.DSL:Register("area.heal.infront", function(unit, args)
+	local total = 0
+	if not _G.UnitExists(unit) then return total end
+	local distance, health = _G.strsplit(",", args, 2)
+	for _,Obj in pairs(NeP.Healing:GetRoster()) do
+		local unit_dist = NeP.Protected.Distance(unit, Obj.key)
+		if unit_dist < (tonumber(distance) or 20)
+		and Obj.health < (tonumber(health) or 100)
+		and NeP.Protected.Infront(unit, Obj.key) then
+			total = total + 1
+		end
+	end
+	return total
+end)
--- system/healing.lua
+++ system/healing.lua
@@ -8,15 +8,15 @@ local forced_role = {
 	[72218] = "TANK" -- Oto the Protector (Proving Grounds)
 }
 
-local function GetPredictedHealth(unit)
+function NeP.Healing.GetPredictedHealth(unit)
 	return _G.UnitHealth(unit)+(_G.UnitGetTotalHealAbsorbs(unit) or 0)+(_G.UnitGetIncomingHeals(unit) or 0)
 end
 
-local function GetPredictedHealth_Percent(unit)
-	return math.floor((GetPredictedHealth(unit)/_G.UnitHealthMax(unit))*100)
+function NeP.Healing.GetPredictedHealth_Percent(unit)
+	return math.floor((NeP.Healing.GetPredictedHealth(unit)/_G.UnitHealthMax(unit))*100)
 end
 
-local function healthPercent(unit)
+function NeP.Healing.healthPercent(unit)
 	return math.floor((_G.UnitHealth(unit)/_G.UnitHealthMax(unit))*100)
 end
 
@@ -24,9 +24,9 @@ end
 local function Add(Obj)
 	local healthRaw = _G.UnitHealth(Obj.key)
 	local maxHealth = _G.UnitHealthMax(Obj.key)
-	Obj.predicted = GetPredictedHealth_Percent(Obj.key)
-	Obj.predicted_Raw = GetPredictedHealth(Obj.key)
-	Obj.health = healthPercent(Obj.key)
+	Obj.predicted = NeP.Healing.GetPredictedHealth_Percent(Obj.key)
+	Obj.predicted_Raw = NeP.Healing.GetPredictedHealth(Obj.key)
+	Obj.health = NeP.Healing.healthPercent(Obj.key)
 	Obj.healthRaw = healthRaw
 	Obj.healthMax = maxHealth
 	Obj.role = forced_role[Obj.id] or _G.UnitGroupRolesAssigned(Obj.key)
@@ -35,10 +35,10 @@ end
 
 local function Refresh(GUID, Obj)
 	local temp = Roster[GUID]
-	temp.health = healthPercent(Obj.key)
+	temp.health = NeP.Healing.healthPercent(Obj.key)
 	temp.healthRaw = _G.UnitHealth(temp.key)
-	temp.predicted = GetPredictedHealth_Percent(Obj.key)
-	temp.predicted_Raw = GetPredictedHealth(Obj.key)
+	temp.predicted = NeP.Healing.GetPredictedHealth_Percent(Obj.key)
+	temp.predicted_Raw = NeP.Healing.GetPredictedHealth(Obj.key)
 	temp.role = forced_role[Obj.id] or _G.UnitGroupRolesAssigned(Obj.key)
 end
 
@@ -76,56 +76,5 @@ end
 
 NeP.Debug:Add("Healing", Iterate, true)
 _G.C_Timer.NewTicker(0.1, Iterate)
-
-NeP.DSL:Register("health", function(target)
-	return healthPercent(target)
-end)
-
-NeP.DSL:Register("health.actual", function(target)
-	return _G.UnitHealth(target)
-end)
-
-NeP.DSL:Register("health.max", function(target)
-	return _G.UnitHealthMax(target)
-end)
-
-NeP.DSL:Register("health.predicted", function(target)
-	return GetPredictedHealth_Percent(target)
-end)
-
-NeP.DSL:Register("health.predicted.actual", function(target)
-	return GetPredictedHealth(target)
-end)
-
--- USAGE: UNIT.area(DISTANCE, HEALTH).heal >= #
-NeP.DSL:Register("area.heal", function(unit, args)
-	local total = 0
-	if not _G.UnitExists(unit) then return total end
-	local distance, health = _G.strsplit(",", args, 2)
-	for _,Obj in pairs(NeP.Healing:GetRoster()) do
-		local unit_dist = NeP.Protected.Distance(unit, Obj.key)
-		if unit_dist < (tonumber(distance) or 20)
-		and Obj.health < (tonumber(health) or 100) then
-			total = total + 1
-		end
-	end
-	return total
-end)
-
--- USAGE: UNIT.area(DISTANCE, HEALTH).heal.infront >= #
-NeP.DSL:Register("area.heal.infront", function(unit, args)
-	local total = 0
-	if not _G.UnitExists(unit) then return total end
-	local distance, health = _G.strsplit(",", args, 2)
-	for _,Obj in pairs(NeP.Healing:GetRoster()) do
-		local unit_dist = NeP.Protected.Distance(unit, Obj.key)
-		if unit_dist < (tonumber(distance) or 20)
-		and Obj.health < (tonumber(health) or 100)
-		and NeP.Protected.Infront(unit, Obj.key) then
-			total = total + 1
-		end
-	end
-	return total
-end)
 
 NeP.Globals.OM.GetRoster = NeP.Healing.GetRoster
