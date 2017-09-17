e8f041377c255a17705038ed14158f45604a4f2c
Jaime Marques <mrthesoulz@gmail.com>
2017-09-17 15:52:43 +0100

    taunt stuff

--- tables/taunts.lua
+++ tables/taunts.lua
@@ -12,28 +12,37 @@ function NeP.Taunts:Add(id, stacks)
   end
 end
 
-function NeP.Taunts:Add(unit)
-  for i=1, #self.table do
-    local debuff = _G.etSpellInfo(self.table[i].id)
-    if not _G.UnitDebuff('player', debuff)
-    and (select(4, _G.UnitDebuff(unit..'target', debuff)) or 0) > self.table[i].stacks then
-      return true
+function NeP.Taunts:ShouldTaunr(unit)
+  --Quit if we have its threat
+  local threat = _G.UnitThreatSituation("player", unit) or 0
+  if threat >= 3 then return end
+  -- Taunts in a raid, where you have 2 tanks
+  if _G.IsInRaid() then
+    for i=1, #self.table do
+      local debuff = _G.etSpellInfo(self.table[i].id)
+      if not _G.UnitDebuff('player', debuff)
+      and (select(4, _G.UnitDebuff(unit..'target', debuff)) or 0) > self.table[i].stacks then
+        return true
+      end
     end
+  -- Alone stuff
+  else
+    return threat > 0 and threat < 3
   end
 end
 
 NeP.Taunts:Add({
-  { id = 143436, stacks = 1 },--143436 - Corrosive Blast (Immerseus/71543)
-  { id = 146124, stacks = 3 },--146124 - Self Doubt (Norushen/72276)
-  { id = 144358, stacks = 1 },--144358 - Wounded Pride (Sha of Pride/71734)
-  { id = 147029, stacks = 3 },--147029 - Flames of Galakrond (Galakras/72249)
-  { id = 144467, stacks = 2 },--144467 - Ignite Armor (Iron Juggernaut/71466)
-  { id = 144215, stacks = 6 },--144215 - Froststorm Strike (Earthbreaker Haromm) (Kor'Kron Dark Shaman/71859)
-  { id = 143494, stacks = 3 },--143494 - Sundering Blow (General Nazgrim/71515)
-  { id = 142990, stacks = 12 },--142990 - Fatal Strike (Malkorok/71454)
-  { id = 143426, stacks = 2 },--143426 - Fearsome Roar (Thok the Bloodthirsty/71529)
-  { id = 143780, stacks = 2 },--143780 - Acid Breath (Thok (Saurok eaten))
-  { id = 143773, stacks = 3 },--143773 - Freezing Breath (Thok (Jinyu eaten))
-  { id = 143767, stacks = 2 },--143767 - Scorching Breath (Thok (Yaungol eaten))
-  { id = 145183, stacks = 3 } --145183 - Gripping Despair (Garrosh/71865)
+  { id = 143436, stacks = 1 },-- Corrosive Blast (Immerseus/71543)
+  { id = 146124, stacks = 3 },-- Self Doubt (Norushen/72276)
+  { id = 144358, stacks = 1 },-- Wounded Pride (Sha of Pride/71734)
+  { id = 147029, stacks = 3 },-- Flames of Galakrond (Galakras/72249)
+  { id = 144467, stacks = 2 },-- Ignite Armor (Iron Juggernaut/71466)
+  { id = 144215, stacks = 6 },-- Froststorm Strike (Earthbreaker Haromm) (Kor'Kron Dark Shaman/71859)
+  { id = 143494, stacks = 3 },-- Sundering Blow (General Nazgrim/71515)
+  { id = 142990, stacks = 12 },-- Fatal Strike (Malkorok/71454)
+  { id = 143426, stacks = 2 },-- Fearsome Roar (Thok the Bloodthirsty/71529)
+  { id = 143780, stacks = 2 },-- Acid Breath (Thok (Saurok eaten))
+  { id = 143773, stacks = 3 },-- Freezing Breath (Thok (Jinyu eaten))
+  { id = 143767, stacks = 2 },-- Scorching Breath (Thok (Yaungol eaten))
+  { id = 145183, stacks = 3 } -- Gripping Despair (Garrosh/71865)
 })
