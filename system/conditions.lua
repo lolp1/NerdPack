9111846ad837b3259109091c663ef1130ac9660f
Jaime Marques <mrthesoulz@gmail.com>
2019-05-17 01:51:17 +0100

    Update conditions.lua

--- system/conditions.lua
+++ system/conditions.lua
@@ -19,7 +19,7 @@ local function _add(name, condition, overwrite)
 	name = name:lower()
 	if not conditions[name] or overwrite then
 		conditions[name] = function(target, spell, spell2, ...)
-			local key = name .. (target or '') .. (spell or spell2 or '')
+			local key = name .. (target or '') .. (spell or spell2 or '') .. (spell2 or '')
 			if C[key] == nil then
 				C[key] = {condition(target, spell or spell2, spell2, ...)}
 			end
