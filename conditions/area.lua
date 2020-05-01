c3d796acf1e9516d1671d135342dcbb1fb2a48f2
Jaime Marques <mrthesoulz@gmail.com>
2020-05-01 11:32:00 +0100

    try Obj.range

--- conditions/area.lua
+++ conditions/area.lua
@@ -7,7 +7,7 @@ NeP.DSL:Register("area.enemies", function(unit, distance)
   for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
     if NeP._G.UnitCanAttack(Obj.key, "player")
 	  and (NeP.DSL:Get('combat')(Obj.key) or Obj.isdummy)
-    and NeP.DSL:Get("range")(unit, nil, Obj.key) < tonumber(distance) then
+    and Obj.range < tonumber(distance) then
       total = total +1
     end
   end
@@ -21,7 +21,7 @@ NeP.DSL:Register("area.enemies.infront", function(unit, distance)
   for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
     if NeP._G.UnitCanAttack(Obj.key, "player")
 	  and (NeP.DSL:Get('combat')(Obj.key) or Obj.isdummy)
-    and NeP.DSL:Get("range")(unit, nil, Obj.key) < tonumber(distance)
+    and Obj.range < tonumber(distance)
     and NeP.DSL:Get("infront")(Obj.key, nil, unit) then
       total = total +1
     end
@@ -34,7 +34,7 @@ NeP.DSL:Register("area.friendly", function(unit, distance)
   if not NeP.DSL:Get('exists')(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
-    if NeP.DSL:Get("range")(unit, nil, Obj.key) < tonumber(distance) then
+    if Obj.range < tonumber(distance) then
       total = total +1
     end
   end
@@ -46,7 +46,7 @@ NeP.DSL:Register("area.friendly.infront", function(unit, distance)
   if not NeP.DSL:Get('exists')(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
-    if NeP.DSL:Get("range")(unit, nil, Obj.key) < tonumber(distance)
+    if Obj.range < tonumber(distance)
     and NeP.DSL:Get("infront")(Obj.key, nil, unit) then
       total = total +1
     end
@@ -59,7 +59,7 @@ NeP.DSL:Register("area.incdmg", function(target, max_dist)
   if not NeP.DSL:Get('exists')(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get("Roster")) do
-    if NeP.DSL:Get("range")(target, nil, Obj.key) < tonumber(max_dist) then
+    if Obj.range < tonumber(max_dist) then
       total = total + NeP.DSL:Get("incdmg")(Obj.key)
     end
   end
@@ -71,7 +71,7 @@ NeP.DSL:Register("area.dead", function(target, max_dist)
   if not NeP.DSL:Get('exists')(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get("Dead")) do
-    if NeP.DSL:Get("range")(target, nil, Obj.key) < tonumber(max_dist) then
+    if Obj.range < tonumber(max_dist) then
       total = total + 1
     end
   end
