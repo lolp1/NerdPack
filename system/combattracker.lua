9387e65dde073c42dac00300843a4135797eb14a
Jaime Marques <mrthesoulz@gmail.com>
2020-05-01 13:50:02 +0100

    oops

--- system/combattracker.lua
+++ system/combattracker.lua
@@ -234,7 +234,6 @@ end)
 NeP.Listener:Add('NeP_CombatTracker_enter_combat', 'UNIT_COMBAT', function(unitid)
 	local DestObj = NeP.OM:FindObjectByGuid(UnitGUID(unitid))
 	DestObj.combat_time = NeP._G.GetTime()
-	DestObj.combat_time = 0
 	DestObj.dmgTaken = 0
 	DestObj.dmgTaken_P = 0
 	DestObj.dmgTaken_M = 0
