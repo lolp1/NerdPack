88f4a933febffff8dff6917929106cf9b67df6f4
Jaime Marques <mrthesoulz@gmail.com>
2020-05-03 21:52:40 +0100

    remove the print

--- system/om.lua
+++ system/om.lua
@@ -303,7 +303,6 @@ local function cleanUnit(Obj)
 		NeP.OM[Obj.tbl][Obj.guid] = Obj
 	end
 	if Obj.name == 'Unknown' then
-		print('found', Obj.key)
 		Obj.name = NeP.DSL:Get('name')(Obj) or 'ERROR!_NO_NAME?'
 	end
 end
