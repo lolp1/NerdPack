6c431af27760a7d0e05dd056d9febf0d8c93e79e
Ramy <11240775+subzrk@users.noreply.github.com>
2020-09-13 14:46:35 +0200

    talents re-work (#346)
    - fixed checking for talents by ID.
    - added support for pvp talents, u can check for them using ID or Name.

--- conditions/talents.lua
+++ conditions/talents.lua
@@ -6,16 +6,23 @@ local rows = 7
 local cols = 3
 
 local function UpdateTalents()
+  -- wipe the table specially for the pvp talents
+  _G.wipe(talents)
   -- this is always 1, dont know why bother but oh well...
   local spec = NeP._G.GetActiveSpecGroup()
+  local pvp = NeP._G.C_SpecializationInfo.GetAllSelectedPvpTalentIDs() -- get a table with all selected pvp talents
   for i = 1, rows do
     for k = 1, cols do
-      local talent_ID, talent_name = NeP._G.GetTalentInfo(i, k, spec)
-      if not talent_name then return end
-      talents[talent_name] = talent_ID
-      talents[talent_ID] = talent_ID
-      talents[tostring(i)..','..tostring(k)] = talent_ID
+      local talent_ID, talent_name, _, selected = NeP._G.GetTalentInfo(i, k, spec)
+      talents[talent_name] = selected
+      talents[tostring(talent_ID)] = selected
+      talents[tostring(i)..','..tostring(k)] = selected
     end
+  end
+  for _, v in pairs(pvp) do
+    local talent_ID, talent_name, _, selected = NeP._G.GetPvpTalentInfoByID(v, spec)
+    talents[talent_name] = selected
+    talents[tostring(talent_ID)] = selected
   end
 end
 
@@ -26,6 +33,11 @@ NeP.Listener:Add('NeP_Talents', 'PLAYER_LOGIN', function()
   end)
 end)
 
+-- Usage: talent(ID/Name/Tier,Column)
+-- /dump NeP.DSL.Parse("talent(23106)", "", "")
+-- /dump NeP.DSL.Parse("talent(Eye of the Tiger)", "", "")
+-- /dump NeP.DSL.Parse("talent(1,1)", "", "")
+-- Note: PvP talents are no longer organized into a row/column so we check for ID/Name
 NeP.DSL:Register("talent", function(_, args)
-  return select(10, NeP._G.GetTalentInfoByID(talents[args], NeP._G.GetActiveSpecGroup()))
+  return talents[args]
 end)
