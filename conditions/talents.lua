f63967a77e225610429703e485c80b26962cafdb
Ramy <11240775+subzrk@users.noreply.github.com>
2020-09-14 16:59:00 +0200

    fix talents errors (#347)

--- conditions/talents.lua
+++ conditions/talents.lua
@@ -6,21 +6,17 @@ local rows = 7
 local cols = 3
 
 local function UpdateTalents()
-  -- wipe the table specially for the pvp talents
-  _G.wipe(talents)
-  -- this is always 1, dont know why bother but oh well...
-  local spec = NeP._G.GetActiveSpecGroup()
-  local pvp = NeP._G.C_SpecializationInfo.GetAllSelectedPvpTalentIDs() -- get a table with all selected pvp talents
+  _G.wipe(talents) -- wipe the table specially for the pvp talents
   for i = 1, rows do
     for k = 1, cols do
-      local talent_ID, talent_name, _, selected = NeP._G.GetTalentInfo(i, k, spec)
+      local talent_ID, talent_name, _, selected = NeP._G.GetTalentInfo(i, k, 1) -- ActiveSpecGroup is always 1
       talents[talent_name] = selected
       talents[tostring(talent_ID)] = selected
       talents[tostring(i)..','..tostring(k)] = selected
     end
   end
-  for _, v in pairs(pvp) do
-    local talent_ID, talent_name, _, selected = NeP._G.GetPvpTalentInfoByID(v, spec)
+  for _, v in pairs(NeP._G.C_SpecializationInfo.GetAllSelectedPvpTalentIDs()) do
+    local talent_ID, talent_name, _, selected = NeP._G.GetPvpTalentInfoByID(v, 1) -- ActiveSpecGroup is always 1
     talents[talent_name] = selected
     talents[tostring(talent_ID)] = selected
   end
@@ -29,7 +25,7 @@ end
 NeP.Listener:Add('NeP_Talents', 'PLAYER_LOGIN', function()
   UpdateTalents()
   NeP.Listener:Add('NeP_Talents', 'ACTIVE_TALENT_GROUP_CHANGED', function()
-    UpdateTalents()
+    NeP._G.C_Timer.After(0.1, function() UpdateTalents() end)
   end)
 end)
 
@@ -39,5 +35,5 @@ end)
 -- /dump NeP.DSL.Parse("talent(1,1)", "", "")
 -- Note: PvP talents are no longer organized into a row/column so we check for ID/Name
 NeP.DSL:Register("talent", function(_, args)
-  return talents[args]
+  return talents[args] ~= nil and talents[args]
 end)
