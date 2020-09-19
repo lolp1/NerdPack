bff186431d825e9c4401a5222bf31d5d406cb030
Ramy <11240775+subzrk@users.noreply.github.com>
2020-09-19 19:49:07 +0200

    fix dbm timers (#348)

--- conditions/dbm.lua
+++ conditions/dbm.lua
@@ -2,19 +2,23 @@ local _, NeP = ...
 NeP.DBM = {}
 
 --dont load if DBM is not installed
-function NeP.DBM.BuildTimers()end
+function NeP.DBM.BuildTimers() end
 if not NeP._G.DBM then return end
 
-NeP.Cache.DBM_Timers = {}
+local DBM_Timers = {}
 local fake_timer = 999
 
 function NeP.DBM.BuildTimers()
+  _G.wipe(DBM_Timers)
   for bar in pairs(NeP._G.DBM.Bars.bars) do
-      local id = NeP._G.GetSpellInfo(bar.id:match("%d+")) or bar.id:lower()
-      NeP.Cache.DBM_Timers[id] = bar.timer and bar.timer
+    local id = NeP._G.GetSpellInfo(bar.id:match("%d+")) or bar.id:lower():gsub("%%s%c", "")
+    DBM_Timers[id] = bar.timer and bar.timer
   end
 end
 
+-- Usage: dbm(Bar name) <= #
+-- /dump NeP.DSL.Parse("dbm(pull in)<=10", "", "")
+-- /dump NeP.DSL:Get('dbm')(_, 'pull in')
 NeP.DSL:Register('dbm', function(_, event)
-  return NeP.Cache.DBM_Timers[event:lower()] or fake_timer
+  return DBM_Timers[event:lower()] or fake_timer
 end)
