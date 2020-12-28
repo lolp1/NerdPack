a62b2f0fe7ab5bc9c731b44c254f1defe6104cd4
Jaime Marques <mrthesoulz@gmail.com>
2020-12-28 05:08:33 +0000

    Update core.lua

--- probably_engine/core.lua
+++ probably_engine/core.lua
@@ -1,12 +1,12 @@
-local _, gbl = ...
+local _, NeP = ...
 
-gbl.PE = {}
+NeP.PE = {}
 
 -- STOP if PE is actualy loaded
 if _G.ProbablyEngine then
-  gbl.core:Print('Found ProbablyEngine, not attaching our wrapper.'
+  NeP.core:Print('Found ProbablyEngine, not attaching our wrapper.'
   .. 'NeP can read ProbablyEngine\'s combat routines if you disable it.')
   return;
 end
 
-_G.ProbablyEngine = gbl.PE
+_G.ProbablyEngine = NeP.PE
