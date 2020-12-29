4ff9e21066be204f5ff2210a868facf0fdd6bcf7
Jaime Marques <mrthesoulz@gmail.com>
2020-12-29 00:56:58 +0000

    Update server-oauth.lua

--- server-oauth.lua
+++ server-oauth.lua
@@ -1,11 +1,11 @@
-  
-local n_name, NeP = ...
-
 local version = 1.4
 local oauthToken;
+-- would like this gone... i need to pass nep tho
+local pointer = tostring(NeP);
+_G[pointer] = NeP;
 
 local function getCrs()
-	print('Loading CRs...', oauthToken)
+	print('Loading CRs...')
 	wmbapi.SendHttpRequest({
 		Url = "http://127.0.0.1:8000/api/user/crs",
 		Method = "GET",
@@ -13,9 +13,10 @@ local function getCrs()
 		Callback = function(request, status)
 			if (status == "SUCCESS") then
 				local _, response = wmbapi.ReceiveHttpResponse(request);
-				RunScript("local NeP = " .. NeP .. ";\n local n_name = " .. n_name .. "\n" .. response.Body)
-			else
-				print('ERROR', status)
+				RunScript("local NeP = _G['" .. pointer .. "'];\n local n_name = '" .. n_name .. "';\n" .. response.Body);
+				NeP.Interface.ResetCRs();
+				NeP.CR:Set();
+				print('Done!')
 			end
 		end
 	});
@@ -31,11 +32,9 @@ local function getToken(username, password)
 		Callback = function(request, status)
 			if (status == "SUCCESS") then
 				local _, response = wmbapi.ReceiveHttpResponse(request);
-				local token = response.Headers:match("Authorization: (.*)$")
+				local token = response.Headers:match("Authorization:%s*(.-)%s")
 				oauthToken = token;
-				getUser()
-			else
-				print('ERROR', status)
+				getCrs()
 			end
 		end
 	});
@@ -44,7 +43,9 @@ end
 local function login()
   local username = NeP.Interface:Fetch(n_name .. '_ServerOAuth', 'username');
   local password = NeP.Interface:Fetch(n_name .. '_ServerOAuth', 'password');
-  getToken(username, password)
+  if password and password:len() > 0 and username and username:len() > 0 then
+  	getToken(username, password)
+  end
 end
 
 local config = {
@@ -62,7 +63,9 @@ local config = {
 	}
 }
 
-
 local GUI = NeP.Interface:BuildGUI(config)
 NeP.Interface:Add('OAuth V:'..version, function() GUI.parent:Show() end)
-GUI.parent:Hide()
+GUI.parent:Hide();
+
+-- try auto login
+login();
