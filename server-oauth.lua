be5786cce1c32121316424e1b1d7602bcf1b9a85
Jaime Marques <mrthesoulz@gmail.com>
2020-12-30 05:23:25 +0000

    Update server-oauth.lua

--- server-oauth.lua
+++ server-oauth.lua
@@ -44,7 +44,7 @@ local function getTokenLB(username, password)
          '{"email": "' .. username.. '", "password": "' .. password .. '"}',
 		 function(content)
 			print(content)
-			local token = content:match("Authorization:%s*(.-)%s")
+			local token = content:match('"token":"(.-)"')
             if not token then
                 print('Ooops, something went wrong. Are your credentials valid?')
             end
