eeabfd7e61181b36b0f575d71e8eeab6270af5ce
jaime <mrthesoulz@gmail.com>
2021-01-10 16:29:51 +0000

    remove debug print

--- Libs/DiesalStyle-1.0/DiesalStyle-1.0.lua
+++ Libs/DiesalStyle-1.0/DiesalStyle-1.0.lua
@@ -31,7 +31,6 @@ local Formatters = DiesalStyle.Formatters
 -- ~~| Locals |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 local OUTLINES = {'_LEFT','_RIGHT','_TOP','_BOTTOM'}
 local MEDIA_PATH = string.format("Interface\\AddOns\\%s\\media\\%s\\",AddonName,MAJOR)
-print(MEDIA_PATH)
 local DEFAULT_COLOR = 'FFFFFF'
 local DEFAULT_GRADIENT_ORIENTATION = 'horizontal'
 local DEFAULT_LAYER = 'ARTWORK'
