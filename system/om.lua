bebc9ac50268aaeb8c66393d0b2d0aa739e046cd
jaime <mrthesoulz@gmail.com>
2021-02-02 02:13:38 +0000

    Update om.lua

--- system/om.lua
+++ system/om.lua
@@ -358,7 +358,7 @@ NeP.Core:WhenInGame(function()
 	NeP.Timer.Add('OM_Maker', NeP.OM.MakerStart, tonumber(F(n_name..'_Settings', 'om_speed_add', 0)))
 	NeP.Timer.Add('nep_OM_sanitizer', NeP.OM.WipeInvalid, tonumber(F(n_name..'_Settings', 'om_speed_sanitize', 0)))
 	NeP.Timer.Add('nep_OM_updateStart', NeP.OM.UpdateStart, tonumber(F(n_name..'_Settings', 'om_speed_update', .5)))
-end, 9999999)
+end, 9997)
 
 NeP.Debug:Add("OM_Clean", NeP.OM.UpdateStart, true)
 NeP.Debug:Add("OM_Maker", NeP.OM.MakerStart, true)
